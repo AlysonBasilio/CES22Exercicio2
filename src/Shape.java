77ba6c4be2ffd8ff598dbc557baadc9fa22c746b
Alyson Fernandes Basilio <alyson.basilio@gmail.com>
2016-04-18 14:26:49 -0300

    Criação da Subclasse Triangle

--- src/Shape.java
+++ src/Shape.java
@@ -16,9 +16,10 @@ public class Shape {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Circle c = new Circle("Circle C");
+		Circle c = new Circle("Circle c");
 		Square s = new Square("Square s");
-		Shape shapeArray[] = {c, s};
+		Triangle t = new Triangle("Triangle t");
+		Shape shapeArray[] = {c, s, t};
 		for (int i=0; i<shapeArray.length; i++){
 			System.out.println("The area of " + shapeArray[i].getName() + " is " + shapeArray[i].calculateArea() + " sq. cm.\n");
 		}
